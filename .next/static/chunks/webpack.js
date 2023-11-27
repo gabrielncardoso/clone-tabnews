45977f658bfb45dd4b7aa04e6c4fcdf332847d50
Gabriel Cardoso <146197942+gabrielncardoso@users.noreply.github.com>
2023-11-27 13:30:59 +0000

    Return edit in index.js

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6cb21ef8a49f07e8"; }
+/******/ 		__webpack_require__.h = function() { return "6b1659170e4ce853"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
