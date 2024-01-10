520c80294b89474f42602d09ed944e093bdb5803
Gabriel Cardoso <146197942+gabrielncardoso@users.noreply.github.com>
2024-01-10 13:18:55 +0000

    configurei o texto da index.js para Página em Construção

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
 function Home() {
-  return <h1>Mensagem de impacto 2</h1>;
+  return (
+    <div className="container">
+      <h1>Página em construção</h1>
+      <p>
+        Estou desenvolvendo esse site como meu primeiro projeto pessoal durante
+        meus estudos de web no curso.dev.
+      </p>
+      <hr />
+      <p>
+        Ainda não defini ao certo qual será o conteúdo, mas uma das ideias é
+        criar um espaço para usuários compartilharem seus projetos de
+        arquitetura.
+      </p>
+    </div>
+  );
 }
 
 export default Home;
